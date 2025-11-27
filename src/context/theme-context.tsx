49accb0f5afecf101195ebeb8ea7c28fa8a42253
saeid rouhparvar <saeid.rohparvar@gmail.com>
2025-11-27 23:29:42 +0330

    create useTheme

---
+++ src/context/theme-context.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { createContext, useContext, useEffect, useState } from "react";
+
+type Theme = "light" | "dark";
+
+interface ThemeContextProps {
+  theme: Theme;
+  toggleTheme: () => void;
+}
+
+const ThemeContext = createContext<ThemeContextProps | undefined>(undefined);
+
+export const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
+  const [theme, setTheme] = useState<Theme>("light");
+
+  // Load from localStorage
+  useEffect(() => {
+    const saved = (localStorage.getItem("theme") as Theme) || "light";
+    setTheme(saved);
+    document.documentElement.classList.toggle("dark", saved === "dark");
+  }, []);
+
+  const toggleTheme = () => {
+    const newTheme: Theme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+    localStorage.setItem("theme", newTheme);
+
+    document.documentElement.classList.toggle("dark", newTheme === "dark");
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+// Custom hook
+export const useTheme = () => {
+  const ctx = useContext(ThemeContext);
+
+  if (!ctx) throw new Error("useTheme must be used inside ThemeProvider");
+
+  return ctx;
+};
