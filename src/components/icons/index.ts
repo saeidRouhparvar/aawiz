61838d6ebe79d30dae0f4a4a052ea1325bedc1be
saeid rouhparvar <saeid.rohparvar@gmail.com>
2025-11-28 15:34:46 +0330

    add new icons

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,11 +1,15 @@
 import { Earth } from "./icons-components/earth";
 import { Moon } from "./icons-components/moon";
+import { Password } from "./icons-components/password";
 import { Sun } from "./icons-components/sun";
+import { User } from "./icons-components/user";
 
 const icons = {
   sun: Sun,
   moon: Moon,
   earth: Earth,
+  user: User,
+  password: Password,
 };
 
 export default icons;
