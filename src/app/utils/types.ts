57ac54bb3bbeceb2f351929751584d07ac835abb
saeid rouhparvar <saeid.rohparvar@gmail.com>
2025-11-30 00:55:20 +0330

    add new types

--- src/app/utils/types.ts
+++ src/app/utils/types.ts
@@ -8,3 +8,17 @@ export type Login = {
   username: string;
   password: string;
 };
+
+export type UserType = {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  phone: string;
+  address: {
+    city: string;
+  };
+  company: {
+    name: string;
+  };
+};
