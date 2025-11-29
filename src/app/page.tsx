e24a6f9cb2762c9e6b2a1f5aaee3e9b907c3b1e0
saeid rouhparvar <saeid.rohparvar@gmail.com>
2025-11-30 00:58:25 +0330

    add splash to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+'use client'
+
+import Splash from "@/components/Splah";
 
 const Home = () => {
-  return (
-    <div>Aawiz</div>
-  )
-}
 
-export default Home
+  return (
+    <div className="p-6 space-y-4">
+      <Splash />
+    </div>
+  );
+};
+
+export default Home;
